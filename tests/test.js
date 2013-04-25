0161b0d25197077016b3e5ce5baa3e5c114c5806
pavelpower <pavel.power@gmail.com>
2013-04-25 11:46:52 +0300

    first refactoring structure of class, make proto, and reform function in proto

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+/**
+ * some test
+ * User: pavpaw
+ * Date: 25.04.13
+ * Time: 11:40
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var c1 = new Color(),
+    c2 = new Color(),
+    c3;
+
+c3 = c1 + c2;
+
+console.log(c3);
